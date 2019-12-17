e62c3a7a06389da4aa3c6d43ae73cae61d1be5d9
User <ivan.nsu.fit@gmail.com>
2019-12-18 00:51:10 +0700

    .

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let email= document.getElementById('email');
 let tel= document.getElementById('tel');
 let url= document.getElementById('url');
 const button= document.querySelector('.form__button');
-let regexName = /^[А-Я][А-Яа-яЁё\-]{2,19}$/gm;
-let regexMail = /[\-\.\w]+@[\-\.\w]+\.[a-z]{2,5}/igm;
-let regexTel = /^(8|\+7)[ ]?\(?\d{3}\)?[\- ]?[\d\- ]{7,10}$/gm;
-let regexUrl = /^(http|https):\/\/(?:[a-z0-9_-]{1,32}(?:[a-z0-9_-]{1,32})?@)?(([a-z0-9-]{1,128}\.)+([a-z\.]{2,6})|(?!0)(?:(?!0[^.]|255)[0-9]{1,3}\.){3}(?!0|255)[0-9]{1,3})(?:\/[a-z0-9.,_@%&?+=\~\/-]*)?(?:#[^ '\"&]*)?$/gm;
+let regexName = /^[А-Я]([А-Яа-яЁё](\-[А-Я])?[А-Яа-яЁё]*){2,19}$/gm;
+let regexMail = /[\-\.\w]+@[\-\.\w]+\.[a-z]{2,}/igm;
+let regexTel = /(^(8|\+7)[0-9]{10}$)|(^\+7(\([0-9]{3}\))( )?[0-9]{3}\-[0-9]{2}\-[0-9]{2}$)|(^\+7( )[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}$)/gm;
+let regexUrl = /(http|https):\/\/(www\.)?(?:[a-z0-9_-]{1,32}(?:[a-z0-9_-]{1,32})?@)?(([a-z0-9-]{1,128}\.)+([a-z\.]{2,6})|(?!0)(?:(?!0[^.]|255)[0-9]{1,3}\.){3}(?!0|255)[0-9]{1,3})(:[0-9]{4})?$/gm;
 
 function reg(rgxp) {
     if (!rgxp.test(String.fromCharCode(event.charCode))) {
